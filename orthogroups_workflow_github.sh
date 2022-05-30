c6b8f3c7eea71d333375023867053acc0ba5e2d6
Nchristo-Bioinformatics <76002285+Nchristo-Bioinformatics@users.noreply.github.com>
2022-05-30 16:22:27 -0400

    Add files via upload
    For running the main parts of the post-Orthofinder Analyses

---
+++ orthogroups_workflow_github.sh
@@ -0,0 +1,8 @@
+grep -e "N18" -e "N23" -e "N27" -e "N30" -e "N33" -e "N36" -e "N38" Duplications_per_Species_Tree_Node.tsv
+#N36     111     64
+#N27     169     29
+#N23    143     7
+N18     57      4
+N38     47      47
+N30     121     13
+N33     245     145
