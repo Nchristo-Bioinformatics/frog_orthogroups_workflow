75b4786c5bddb7b77e26661dd75c3b0e846a3edd
Nchristo-Bioinformatics <76002285+Nchristo-Bioinformatics@users.noreply.github.com>
2022-06-02 13:22:49 -0400

    Create orthogroups_workflow_github.sh

--- orthogroups_workflow_github.sh
+++ orthogroups_workflow_github.sh
@@ -1,3 +1,38 @@
+####Run first orthofinder to make orthogroups###
+./orthofinder -t 20 -a 16 -f NO_CDHIT_ORTHOGROUPS_CHECKING -T fasttree -M msa
+
+####Getting orthogroups with at least two pristimantis sequences, for filtering with phylopypruner####
+####getting the trees for phylopypruner###
+awk '{nzeros=0; for(col=36; col<=NF; col++) {if($col == 0) {nzeros++}} {if(nzeros < 6) {print $0}}}' Orthogroups.GeneCount.tsv | cut -f1 | while read -r LINE; do cp ../OrthoFinder/Results_May17/Resolved_Gene_Trees/$LINE* OG_trees/; done
+###getting the orthofinder alignnments for phylopypruner####
+awk '{nzeros=0; for(col=36; col<=NF; col++) {if($col == 0) {nzeros++}} {if(nzeros < 6) {print $0}}}' Orthogroups.GeneCount.tsv | cut -f1 | while read -r LINE; do cp ../OrthoFinder/Results_May17/MultipleSequenceAlignments/$LINE* OG_pep_alignments/; done
+###getting the original fastas for aligning with pal2nal####
+awk '{nzeros=0; for(col=36; col<=NF; col++) {if($col == 0) {nzeros++}} {if(nzeros < 6) {print $0}}}' Orthogroups.GeneCount.tsv | cut -f1 | while read -r LINE; do cp ../OrthoFinder/Results_May17/Orthogroup_Sequences/$LINE* OG_fastas/; done
+
+###Put all the alignments and trees into a directory, run phylopypruner on the directory as so####
+phylopypruner --overwrite --dir filtering_seqs_orthofinder_alignments/ --output pruning_output_2/ --threads 16 --include Oreobates_cruralis mzua2401_Trinity mzua2448_Trinity mzua2499_Trinity mzua2511_Trinity mzua2538_Trinity mzua2591_Trinity mzua2610_Trinity --subclades subclades.txt --trim-divergent .80 --trim-lb 5 --min-taxa 20
+
+###make a new species tree from concatenated phylopypruner alignment, will be using this to re-run orthofinder###
+
+iqtree -s supermatrix.fas -pre supermatrix_mzuaGTR20 -nt 10 -m GTR20 -bb 5000 -o Bombina_orientalis,Bombina_bombina,Bombina_variegata
+
+
+
+####re-run orthofinder with your new species tree, to make sure that duplications are correctly put on the tree####
+./orthofinder -t 20 -a 16 -f NO_CDHIT_NEW_TREE_CHECK -s supermatrix_mzuaGTR20.contree_rooted -T fasttree -M msa
+
+
+
+
+
+
+
+####remove all phylopypruner alignments without mzua in them####
+grep -r -L -Z 'mzua' . | xargs --null rm
+sed -i 's/|/_/g' *fa
+sed -i 's/_lcl_/_lcl|/g' *fa
+
+
 ###Getting duplicated orthogroups###
 
 grep -e "N18" -e "N23" -e "N27" -e "N30" -e "N33" -e "N36" -e "N38" Duplications_per_Species_Tree_Node.tsv
@@ -17,27 +52,6 @@ awk '$2 == "N33"' Duplications.tsv | cut -f1,2,3,4 | sort -k4 -n | tail -n145 | 
 awk '$2 == "N36"' Duplications.tsv | cut -f1,2,3,4 | sort -k4 -n | tail -n64 | sort -u -k1,1  >> ALL_dupgroups_nodes_support.txt
 awk '$2 == "N38"' Duplications.tsv | cut -f1,2,3,4 | sort -k4 -n | tail -n47 | sort -u -k1,1 >> ALL_dupgroups_nodes_support.txt
 
-####Getting orthogroups with at least two pristimantis sequences, for filtering with phylopypruner####
-####getting the trees for phylopypruner###
-awk '{nzeros=0; for(col=36; col<=NF; col++) {if($col == 0) {nzeros++}} {if(nzeros < 6) {print $0}}}' Orthogroups.GeneCount.tsv | cut -f1 | while read -r LINE; do cp ../OrthoFinder/Results_May17/Resolved_Gene_Trees/$LINE* OG_trees/; done
-###getting the orthofinder alignnments for phylopypruner####
-awk '{nzeros=0; for(col=36; col<=NF; col++) {if($col == 0) {nzeros++}} {if(nzeros < 6) {print $0}}}' Orthogroups.GeneCount.tsv | cut -f1 | while read -r LINE; do cp ../OrthoFinder/Results_May17/MultipleSequenceAlignments/$LINE* OG_pep_alignments/; done
-###getting the original fastas for aligning with pal2nal####
-awk '{nzeros=0; for(col=36; col<=NF; col++) {if($col == 0) {nzeros++}} {if(nzeros < 6) {print $0}}}' Orthogroups.GeneCount.tsv | cut -f1 | while read -r LINE; do cp ../OrthoFinder/Results_May17/Orthogroup_Sequences/$LINE* OG_fastas/; done
-
-###Put all the alignments and trees into a directory, run phylopypruner on the directory as so####
-phylopypruner --overwrite --dir filtering_seqs_orthofinder_alignments/ --output pruning_output_2/ --threads 16 --include Oreobates_cruralis mzua2401_Trinity mzua2448_Trinity mzua2499_Trinity mzua2511_Trinity mzua2538_Trinity mzua2591_Trinity mzua2610_Trinity --subclades subclades.txt --trim-divergent .80 --trim-lb 5 --min-taxa 20
-
-###take the output alignment from phylopypruner, make a new species tree with IQtree###
-iqtree -s supermatrix.fas -pre supermatrix_mzuaGTR20 -nt 10 -m GTR20 -bb 5000 -o Bombina_orientalis,Bombina_bombina,Bombina_variegata
-
-###make da nucleotide files from your csvs####
-for file in *.csv; do f2=${file%%.csv}".nuc.fa"; seqtk subseq bigtranscripts_1.fasta $file > ../reduced_nucs_unaligned/$f2; done
-
-####remove all phylopypruner alignments without mzua in them####
-grep -r -L -Z 'mzua' . | xargs --null rm
-sed -i 's/|/_/g' *fa
-sed -i 's/_lcl_/_lcl|/g' *fa
 
 
 #########Get protein sequences for your selected proteins from the original, unaligned protein fasta orthogroup files from Orthofinder###
@@ -47,3 +61,7 @@ for file in *pep.fa; do f2=${file%%pep.fa}"pep.aln"; mafft --anysymbol --auto --
 
 ####Now make nucleotide alignment files from your pruned nucleotide fastas, and your aligned, pruned protein fastas you just made####
 for file in *_pruned.aln; do f2=${file%%_pruned.aln}"_pruned.nuc.fa"; f3=${file%%_pruned.aln}".nuc.aln"; pal2nal.pl $file ../pruned_nucs/$f2 -output fasta > ../nuc_alignments_from_pal2nal/$f3; done
+###trim your nucleotide alignments###
+for file in *aln; do f2=${file%%.aln}"trim.aln"; trimal -in $file -out ../trimmed_nuc_alignments_pal2nal/$f2 -fasta -gappyout; done
+####now remove stop codons####
+hyphy ~/miniconda3/envs/dammit/lib/hyphy/TemplateBatchFiles/CleanStopCodons.bf Universal OG0001347.alntrim No/Yes OG0001347.alntrim_nostopls
