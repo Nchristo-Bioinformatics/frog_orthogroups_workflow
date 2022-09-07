12ea34ce7744d81fdf03f74925b9f42437618457
Nchristo-Bioinformatics <76002285+Nchristo-Bioinformatics@users.noreply.github.com>
2022-09-06 20:34:31 -0400

    Update Dup_orthogroups_workflow.sh

--- Duplicated_orthogroups/Dup_orthogroups_workflow.sh
+++ Duplicated_orthogroups/Dup_orthogroups_workflow.sh
@@ -1,35 +1,36 @@
-grep -e "N23" -e "N27" -e "N30" -e "N32" -e "N35" -e "N37" -e "N39" Duplications_per_Species_Tree_Node.tsv
-#N32     121     13
-#N27     143     7
-#N23     58      4
-#N39     45      45
-#N30     169     29
-#N35     245     145
-#N37     113     55
+grep -e "N21 -e "N24" -e "N27"-e "N30"-e "N33 -e "N36" -e "N38" Duplications_per_Species_Tree_Node.tsv
+#N36     36      16
+#N30     36      3
+#N21     70      1
+#N24     89      4
+#N33     90      42
+#N27     87      10
+#N38     11      11
 
 
 
-awk '$2 == "N23"' Duplications.tsv | cut -f1,2,3,4 | sort -k4 -n | tail -n4 | sort -u -k1,1 >> ALL_dupgroups_nodes_support.txt
-awk '$2 == "N27"' Duplications.tsv | cut -f1,2,3,4 | sort -k4 -n | tail -n7 | sort -u -k1,1 >> ALL_dupgroups_nodes_support.txt
-awk '$2 == "N30"' Duplications.tsv | cut -f1,2,3,4 | sort -k4 -n | tail -n29 | sort -u -k1,1 >> ALL_dupgroups_nodes_support.txt
-awk '$2 == "N32"' Duplications.tsv | cut -f1,2,3,4 | sort -k4 -n | tail -n13 | sort -u -k1,1  >> ALL_dupgroups_nodes_support.txt
-awk '$2 == "N35"' Duplications.tsv | cut -f1,2,3,4 | sort -k4 -n | tail -n145 | sort -u -k1,1  >> ALL_dupgroups_nodes_support.txt
-awk '$2 == "N37"' Duplications.tsv | cut -f1,2,3,4 | sort -k4 -n | tail -n55 | sort -u -k1,1 >> ALL_dupgroups_nodes_support.txt
-awk '$2 == "N39"' Duplications.tsv | cut -f1,2,3,4 | sort -k4 -n | tail -n45 | sort -u -k1,1 >> ALL_dupgroups_nodes_support.txt
+awk '$2 == "N21"' Duplications.tsv | cut -f1,2,3,4 | sort -k4 -n | tail -n1 | sort -u -k1,1 >> ALL_dupgroups_nodes_support.txt
+awk '$2 == "N24"' Duplications.tsv | cut -f1,2,3,4 | sort -k4 -n | tail -n4 | sort -u -k1,1 >> ALL_dupgroups_nodes_support.txt
+awk '$2 == "N27"' Duplications.tsv | cut -f1,2,3,4 | sort -k4 -n | tail -n10 | sort -u -k1,1 >> ALL_dupgroups_nodes_support.txt
+awk '$2 == "N30"' Duplications.tsv | cut -f1,2,3,4 | sort -k4 -n | tail -n3 | sort -u -k1,1  >> ALL_dupgroups_nodes_support.txt
+awk '$2 == "N33"' Duplications.tsv | cut -f1,2,3,4 | sort -k4 -n | tail -n42 | sort -u -k1,1  >> ALL_dupgroups_nodes_support.txt
+awk '$2 == "N36"' Duplications.tsv | cut -f1,2,3,4 | sort -k4 -n | tail -n16 | sort -u -k1,1 >> ALL_dupgroups_nodes_support.txt
+awk '$2 == "N38"' Duplications.tsv | cut -f1,2,3,4 | sort -k4 -n | tail -n11 | sort -u -k1,1 >> ALL_dupgroups_nodes_support.txt
+
 
 #############How many duplicated orthogroups?############################
 cut -f1 ALL_dupgroups_nodes_support.txt | sort | uniq | wc -l
-#236
+
+#####Let's gather the protein fastas for duplicated orthogroups###
+mkdir 01-SEQS
+cut -f1 ALL_dupgroups_nodes_support.txt | sort | uniq | while read -r LINE; do cp ../Orthofinder/01-AA_cdhit/OrthoFinder/Results_Jul29/Orthogroup_Sequences/$LINE".fa" 01-SEQS/; done
 
 ####how many have an annotation?########
-cut -f1 ALL_dupgroups_nodes_support.txt | sort | uniq | while read -r LINE; do grep $LINE cluster_domain_annotation.* >> dupgroup_kinfin_annotations.txt; done
-cut -f1 dupgroup_kinfin_annotations.txt | cut -f2 -d: | sort | uniq | wc -l
-##228###
+cut -f1 ALL_dupgroups_nodes_support.txt | sort | uniq | while read -r LINE; do grep $LINE all_annotations.tsv >> Pristi_oreo_dupgroup_kinfin_annotations.txt; done
 
-###Gather the original orthofinder fastas####
-mkdir OGfastas
-cut -f1 dupgroup_kinfin_annotations.txt | cut -f2 -d: | sort | uniq| while read -r LINE; do cp ~/OrthoFinder/NO_CDHIT_NEW_TREE_CHECK/OrthoFinder/Results_Jun01/Orthogroup_Sequences/$LINE*".fa" OGfastas/; done
+
 ###PRUNING PARALOGUES TO MAKE ORTHOGROUPS SMALLER###
+
 mkdir OG_alignments
 cut -f1 dupgroup_kinfin_annotations.txt | cut -f2 -d: | sort | uniq| while read -r LINE; do cp ~/OrthoFinder/NO_CDHIT_NEW_TREE_CHECK/OrthoFinder/Results_Jun01/MultipleSequenceAlignments/$LINE* OG_alignments/; done
 cut -f1 dupgroup_kinfin_annotations.txt | cut -f2 -d: | sort | uniq| while read -r LINE; do cp ~/OrthoFinder/NO_CDHIT_NEW_TREE_CHECK/OrthoFinder/Results_Jun01/Resolved_Gene_Trees/$LINE* OG_alignments/; done
