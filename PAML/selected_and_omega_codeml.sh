0b5d59dd4de2418322d56f3839e424d9d3311273
Nchristo-Bioinformatics <76002285+Nchristo-Bioinformatics@users.noreply.github.com>
2022-10-24 13:41:58 -0400

    Update selected_and_omega_codeml.sh

--- PAML/selected_and_omega_codeml.sh
+++ PAML/selected_and_omega_codeml.sh
@@ -4,3 +4,15 @@ for file in *codeml; do grep -A2 "proportion" $file  | sed '/background/d'| cut 
 cat *props_omegas.csv > fore_back_props_omegas.csv
 
 ####make this the header for fore_back_props_omegas.csv#### ---> prop_2a,prop_2b,foreground_background,omega_2a,omega_2b
+
+
+###for t-tests on proportions of selected sites, omegas for each codeml hypothesis###
+for file in *codeml; do echo $file; grep -e proportion -e "background w" $file | sed 's/background w/background.w/g' | \
+sed 's/proportion /proportion/g' | sed 's/  / /g' | cut -f1,6,7,8 -d" " | cut -f2,3 -d" " | paste -d " " - - | while read -r pine; 
+do echo "$file $pine background" | sed 's/.nuc.nostop.trim.fa.alt.codeml//g' | sed 's/ /,/g'  >> pristi_fore_back_props_omegas.csv; done; done
+
+for file in *codeml; do echo $file; grep -e proportion -e "foreground w" $file | sed 's/foreground w/foreground.w/g' | \
+sed 's/proportion /proportion/g' | sed 's/  / /g' | cut -f1,6,7,8 -d" " | cut -f2,3 -d" " | paste -d " " - - | while read -r pine; 
+do echo "$file $pine foreground" | sed 's/.nuc.nostop.trim.fa.alt.codeml//g' | sed 's/ /,/g'  >> pristi_fore_back_props_omegas.csv; done; done
+
+##add headers to your files##
